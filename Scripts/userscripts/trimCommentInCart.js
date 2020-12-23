86940065ad7b4f8b5b786c260e68cc5ccc486f10
dmitryaksenov99 <76534329+dmitryaksenov99@users.noreply.github.com>
2020-12-23 23:03:04 +0300

    Add files via upload

---
+++ Scripts/userscripts/trimCommentInCart.js
@@ -0,0 +1,14 @@
+﻿try {
+    var length = App.orderComment.value.length;
+
+    if (length > 100) {
+        App.orderComment.setValue(App.orderComment.value.substring(0, length - 1));  
+        App.symbolsCount.setText('Осталось 0 символов из 100');
+    }
+    else {
+        App.symbolsCount.setText('Осталось ' + (100 - length) + ' символов из 100');
+    }
+}
+catch {
+
+}
---
+++ Scripts/userscripts/trimCommentInOrder.js
@@ -0,0 +1,11 @@
+﻿try {
+    if (App.orderComment.text.length > 100) {
+        App.orderComment.setText(App.orderComment.text.substring(0, 100) + '...')
+    }
+    else {
+
+    }
+}
+catch {
+
+}
