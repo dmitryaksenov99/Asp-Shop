0a003c5087f096418817f1f40018b6f1e85a544e
dmitryaksenov99 <76534329+dmitryaksenov99@users.noreply.github.com>
2020-12-23 22:57:27 +0300

    Add files via upload

---
+++ App_Start/IdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using AspShop.Helpers;
+using AspShop.Helpers.App;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace AspShop.App_Start
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(AppDbContext.Create);
+            app.CreatePerOwinContext<AppUserManager>(AppUserManager.Create);
+            app.CreatePerOwinContext<AppRoleManager>(AppRoleManager.Create);
+            app.CreatePerOwinContext<AppSignInManager>(AppSignInManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+    }
+}
